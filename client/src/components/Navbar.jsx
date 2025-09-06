09bfeba4530f980cd655fff97951161705086379
Abir-dev <abirlalbanerjee44@gmail.com>
2025-09-06 21:12:22 +0530

    logo issue fixation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import LoginModal from "./LoginModal.jsx";
 import SignupModal from "./SignupModal.jsx";
-import logo from "../assets/logo4.png";
 
 const navLink =
   "uppercase text-lg font-bold tracking-wide text-slate-700 hover:text-[#1B4A8B] transition-colors px-4 py-2";
@@ -42,7 +41,7 @@ export default function Navbar() {
             {/* Logo */}
             <Link to="/" className="flex items-center gap-2">
               <img
-                src={logo}
+                src="/logo4.png"
                 alt="King Logo"
                 className="h-15 w-15 invert"
               />
